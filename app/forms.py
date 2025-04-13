c93e16e1e2681a64cb16d3a95c522d6ac232385a
Alaynia Patterson <alayniapatterson1100@gmail.com>
2025-04-12 21:13:57 -0500

    Exercise 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,16 @@
 # Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_wtf.file import FileAllowed
+
+# Custom validator to check if file is an image
+def validate_image(form, field):
+    if field.data:
+        if not field.data.filename.endswith('.jpg') and not field.data.filename.endswith('.png'):
+            raise ValidationError('Only JPG and PNG images are allowed.')
+
+class MovieForm(FlaskForm):
+    title = StringField('Movie Title', validators=[InputRequired(), Length(max=100)])
+    description = TextAreaField('Description', validators=[InputRequired()])
+    poster = FileField('Movie Poster', validators=[InputRequired(), FileAllowed(['jpg', 'png']), validate_image])
